b0d318415b9a1f7338add958faf16a21d58a9e91
mappmechanic <rahat.khanna@genpact.com>
2013-12-10 18:19:52 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+ * ==================================================
+ *            MySQL Proxy Server
+ * ==================================================
+ * A proxy Middleware Server to mediate MySQL DB Administration using a REST API. The Rest API can be used to build a * Web/Mobile based Administration Console to run Queries.
+ * Author: Rahat Khanna
+ * Date: 10th December 2013
+*/
+
+var express = require('express');
